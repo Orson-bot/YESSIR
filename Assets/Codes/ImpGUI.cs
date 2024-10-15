fccb2f868d4cd674ec80f59a1118517b125e1aae
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-10-15 14:24:28 +0800

    qdwef vwe

--- Assets/Codes/ImpGUI.cs
+++ Assets/Codes/ImpGUI.cs
@@ -1,31 +1,51 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using System.Linq.Expressions;
 public class ImpGUI : MonoBehaviour
 {
-    public string labelText = "Hello";
+
+    public string text = "Stat check bro";
+    private int agep1 = 127;
+    private int attackp1 = 1001;
+    private int defp1 = 23;
+    private int votesp1 = 102300;
+
+
+    private int agep2 = 666;
+    private int attackp2 = 0;
+    private int defp2 = 82;
+   private int votesp2 = 1300;
+
+    private int agep3 = 56;
+    private int attackp3 = 13213;
+    private int defp3 = 2000;
+    private int votesp3 = 30112;
+
     private void OnGUI()
     {
-        GUI.Box(new Rect(30, 40, 1400, 650), "Change Statistics");
+        GUI.Box(new Rect(227, 120, 995, 400), "Options");
 
-       if (GUI.Button(new Rect(50,100, 100, 100), "Player 1"))
+        if (GUI.Button(new Rect(260, 145, 90, 80), "Player One"))
         {
-            labelText = "Name : Rastaman\n\rAttack:7642\n\r Defense : 0\n\r Speed:8\n\r Charimsa: 10000000\n\r Influence: 9999999999\n\r Votes: 2890439";
+            print("Player One");
+            text = $"Name: Diwata Pares \r\n Race : Filipino \r\n Age: {agep1} \r\n Attack : {attackp1} \r\n DEFENSE : {defp1} \r\n Votes : {votesp1}";
+        }
+        if (GUI.Button(new Rect(260, 275, 90, 80), "Player Two"))
+        {
+            print("pTwo");
+            text = $"Name: BBM \r\n Race : Aien \r\n Age: {agep2} \r\n Attack : {attackp2} \r\n DEFENSE : {defp2} \r\n Votes : {votesp2}"; 
+        }
+        if (GUI.Button(new Rect(260, 415, 90, 80), "Player Three"))
+        {
+            print("pThree");
+            text = $"Name: Rastaman \r\n Race : SuperHuman \r\n Age: {agep3} \r\n Attack : {attackp3} \r\n DEFENSE : {defp3} \r\n Votes : {votesp2}"; 
+        }
 
-        }
-        if (GUI.Button(new Rect(50, 300, 100, 100), "Player 2"))
-        {
-            print("p2");
-            labelText = "Name : Ferdinand Romualdez  Marcos Jr.\n\r Attack:-100000\n\r Defense : 10000000\n\r Speed:-800\n\r Charimsa: -99999999\n\r Influence: 100\n\r Votes: 11700000";
-
-        }
-        if (GUI.Button(new Rect(50, 500, 100, 100), "Player 3"))
-        {
-            print("p3");
-            labelText = "Name : Diwata Pares\n\r Attack:77777777\n\r Defense : 8080\n\r Speed:99999999\n\r Charimsa: 9975763\n\r Influence: 1100010\n\r Votes: 10987";
-        }
-        GUI.Label(new Rect(500, 100, 200, 300), labelText);
+        GUI.Label(new Rect(750, 250, 200, 200), text);
     }
+
+
 }
